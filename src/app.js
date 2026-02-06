a9df5f1834d3f22353c256bfe30cd643bdb93a5c
Matias Otero Sarra <76122487+MatOtS@users.noreply.github.com>
2026-02-06 19:03:41 +0000

    Ejercicio base

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+const palos = ["♦","♥","♠","♣"]
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomCards(arr){
+    let randObject = arr[Math.floor(Math.random()*arr.length)]
+    return randObject
+  }
+
+  let cardValue = document.querySelector("#cardValue")
+  let paloT = document.querySelector("#paloT")
+  let paloB = document.querySelector("#paloB")
+
+  cardValue.innerHTML = randomCards(values)
+  let newPalo = randomCards(palos)
+  paloT.innerHTML = newPalo;
+  paloB.innerHTML = newPalo;
+  if (newPalo=== "♥" || newPalo === "♦" ) {
+    paloT.classList.add('reds');
+    paloB.classList.add('reds');
+  }
+
 };
